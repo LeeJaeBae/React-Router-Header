f9deea76eea2c5354d867c5ac7d85fc72e3647e0
LeeJaeBae <nea4182@gmail.com>
2021-01-30 21:27:58 +0900

    ts config

---
+++ Test/test.test.ts
@@ -0,0 +1,6 @@
+describe("test", () => {
+    test('test', () => {
+        const A: string = "test";
+        expect(A).toBe("test");
+    })
+})
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('test');
+//# sourceMappingURL=index.js.map
